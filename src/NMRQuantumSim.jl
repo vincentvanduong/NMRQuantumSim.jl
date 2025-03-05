602a2ee230403cbd63376751e4284b21def4516e
Van Vincent Duong <vince.duong97@gmail.com>
2025-03-05 13:35:35 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.53

---
+++ src/NMRQuantumSim.jl
@@ -0,0 +1,5 @@
+module NMRQuantumSim
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NMRQuantumSim
+using Test
+
+@testset "NMRQuantumSim.jl" begin
+    # Write your tests here.
+end
